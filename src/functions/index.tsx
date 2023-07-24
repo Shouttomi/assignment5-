47aa55508e0a2a3dc2213c9ebd0a274d75f4def8
Shouttomi <omiathak2002@gmail.com>
2023-07-25 01:11:18 +0530

    probfinal2

--- src/functions/index.tsx
+++ src/functions/index.tsx
@@ -1,6 +1,6 @@
 import { JsonConverter } from "./Jsonconverter";
 import {   helper as Subdepthelper} from "./Subdepthelper";
 import {countAllSubitems} from './Countsubitems'
-import { Fetch } from "./Fetch";
 
-export { JsonConverter, Subdepthelper,countAllSubitems ,Fetch};
+
+export { JsonConverter, Subdepthelper,countAllSubitems };
